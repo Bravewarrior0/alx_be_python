2771021f947d9c39f95cb60e40087996781f4c1c
bravewarrior0 <bravewarrior1997@gmail.com>
2024-09-14 16:06:48 +0300

    0. Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+diff = number1 - number2
+product = number1 * number2
+print("Addition of {} and {} is {}".format(number1, number2, sum))
+print("Subtraction of {} and {} is {}".format(number1, number2, diff))
+print("Multiplication of {} and {} is {}".format(number1, number2, product))
